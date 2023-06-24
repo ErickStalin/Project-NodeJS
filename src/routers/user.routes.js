5f46f42bb403ca3ad3daa42930a823282539be77
Erick <erick.santillan@epn.edu.ec>
2023-06-24 12:18:13 -0500

    Project with node.js and express

---
+++ src/routers/user.routes.js
@@ -0,0 +1,15 @@
+//Invocar la funcion router
+const {Router} = require('express')
+//Invocar las funciones del controlador
+const { renderRegisterForm, registerNewUser, renderLoginForm, loginUser, logoutUser,confirmEmail} = require('../controllers/user.controller')
+//Inicializar la funcion de la variable router
+const router = Router()
+//Definir las rutas
+router.get('/user/confirmar/:token',confirmEmail)
+router.get('/user/register',renderRegisterForm)
+router.post('/user/register',registerNewUser)
+router.get('/user/login',renderLoginForm)
+router.post('/user/login',loginUser)
+router.post('/user/logout',logoutUser)
+
+module.exports =router
