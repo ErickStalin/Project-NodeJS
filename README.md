19caf5a9796d838f6f83898beadc6d2569699633
Elian027 <elian.moreira@epn.edu.ec>
2023-06-27 04:37:44 -0500

    Datos en base

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Login
+
+![image](https://github.com/ErickStalin/Project-NodeJS/assets/117755180/405c6c41-e43d-4692-898a-45db7d5e93f6)
+
+# Portafolios
+
+![image](https://github.com/ErickStalin/Project-NodeJS/assets/117755180/c38d08bf-921b-41b8-b559-43853c961da7)
+
+# Perfil
+
+![image](https://github.com/ErickStalin/Project-NodeJS/assets/117755180/4f50c3f5-4503-4550-882a-3e3e3532943d)
+
+# Editar Perfil
+
+![image](https://github.com/ErickStalin/Project-NodeJS/assets/117755180/bbfa7aa0-2ed7-46b5-a20f-b8c37d060b8a)
---
+++ src/controllers/perfil.controller.js
@@ -0,0 +1,37 @@
+
+const User = require('../models/User') //importar modelo
+const fs = require('fs-extra')
+
+const renderPerfil = async (req,res) => {
+    const USER = await User.findById(req.user._id).lean()
+    console.log(USER)
+    res.render('user/Perfil',{USER})
+}
+
+const renderEdit = async (req,res) => {
+    const editarP = await User.findById(req.user._id).lean()
+    res.render('user/editarPerfil',{editarP})
+    
+    console.log("wwwwwwww-------",req.params._id)
+    console.log("editarP: ", editarP)
+}
+
+const updateEdit = async (req,res) => {
+    const editarP = await User.findById(req.user._id).lean()
+    if(editarP._id != req.user._id) return res.redirect('user/perfil')
+    console.log("editarP de update: ",{editarP})
+    const data ={
+        names:req.body.names || editarP.names,
+        date: req.body.date || editarP.date,
+        location:req.body.location || editarP.location,
+        ocupation:req.body.ocupation || editarP.ocupation
+    }
+    await editarP.findByIdAndUpdate(req.user._id,{names,date,location,ocupation})
+    res.redirect('user/perfil')
+}
+
+module.exports={
+    renderPerfil,
+    renderEdit,
+    updateEdit
+}
